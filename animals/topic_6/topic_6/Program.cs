1e130fd2452362209664ed194399505a7a0b9225
MaxKlenin <72246785+MaxKlenin@users.noreply.github.com>
2021-02-24 15:41:24 +0200

    Add files via upload

---
+++ animals/topic_6/topic_6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace topic_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100000;
+            int[] arr = new int[n];
+            long ellapledTicks;
+            int x = 123;
+            double sum = 0;
+            object[] o = new object[n];
+
+
+            Stopwatch stopwatch = new Stopwatch();
+            Console.WriteLine("Измерение времени выполнения при помощи класса Stopwatch");
+            Console.WriteLine("****измеряем скорость boxing****") ;
+            stopwatch.Start();
+            for (int i = 0; i < n; i++)
+            {              
+                o[i] = i;
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Средняя скорость операции =" + Convert.ToDouble(stopwatch.ElapsedTicks)/n);
+            stopwatch.Reset();
+           
+            Console.WriteLine("****измеряем скорость unboxing****");
+            stopwatch.Start();
+            for (int i = 0; i < n; i++)
+            {
+                x = (int)o[i];
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Средняя скорость операции =" + Convert.ToDouble(stopwatch.ElapsedTicks) / n);
+            stopwatch.Reset();
+
+            Console.WriteLine("****измеряем скорость присваивания****");
+            stopwatch.Start();
+            for (int i = 0; i < n; i++)
+            {
+                x = 1;
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Средняя скорость операции =" + Convert.ToDouble(stopwatch.ElapsedTicks) / n);
+            stopwatch.Reset();
+        }
+    }
+}
