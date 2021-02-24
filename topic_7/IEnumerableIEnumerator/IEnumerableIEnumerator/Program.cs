0b312cdf2d5256426f45e57b05926af04a2027fc
MaxKlenin <72246785+MaxKlenin@users.noreply.github.com>
2021-02-24 17:40:16 +0200

    Add files via upload

---
+++ topic_7/IEnumerableIEnumerator/IEnumerableIEnumerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableIEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit[] fruits = {new Fruit("Яблоко", 15), new Fruit("Груша", 5), 
+                new Fruit("Ананас", 16), new Fruit("Апельсин", 2)};
+
+            Basket basket = new Basket(fruits);
+            foreach (var item in basket)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            var enumerator = basket.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+    }
+}
